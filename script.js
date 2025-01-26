f78bdc48cdaaea251ee85143643be994e41f39b3
KaueSverberi <miletkaue@gmail.com>
2025-01-26 15:52:01 -0300

    mudança no codigo

--- script.js
+++ script.js
@@ -2,22 +2,38 @@ let input = document.querySelector("input")
 let p = document.querySelector("p")
 
 const contatos = [
-    {name: "Mae", number: "1298128-0328" },
-    {name: "Kevin", number: "1299106-2204" },
-    {name: "Kaue", number: "12991708290" },
-    {name: "Vo Kaka", number: "1299663-8004" },
-    {name: "Vo Kika", number: "12 99670-9648" },
+    { name: "Mae", number: "1298128-0328" },
+    { name: "Kevin", number: "1299106-2204" },
+    { name: "Kaue", number: "12991708290" },
+    { name: "Vo Kaka", number: "1299663-8004" },
+    { name: "Vo Kika", number: "12 99670-9648" },
 ]
 
-function procurar(){
-    
-    for( let i = 0; i < contatos.length; i++){
-        if(input.value.toLowerCase() === contatos[i].name.toLowerCase()){
+function procurar() {
+    let i = 0
+
+    while (i < contatos.length) {
+        if (input.value.toLowerCase() === contatos[i].name.toLowerCase()) {
             p.innerHTML = `Nome: ${contatos[i].name} / Número: ${contatos[i].number}`
 
             break
-        }else {
+        } else {
             p.innerHTML = "Contato não encontrado"
         }
+        i++
     }
 }
+
+
+
+
+
+//for(const contact of contatos){
+//    if(input.value.toLowerCase() === contact.name.toLowerCase()){
+//       p.innerHTML = `Nome: ${contact.name} / Número: ${contact.number}`
+//
+//       break
+//   }else {
+//        p.innerHTML = "Contato não encontrado"
+//    }
+//}
